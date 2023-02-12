3f0416e8724036481af478c0492d4efe72221047
cmelgreen <cmelgreen@gmail.com>
2023-02-11 20:35:05 -0600

    fix cloudfront config

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -6,15 +6,58 @@ resource "aws_s3_bucket" "cloud_resume" {
   bucket = "${var.PROJECT_OWNER}-${var.PROJECT_NAME}"
 }
 
+data "aws_acm_certificate" "issued" {
+  domain   = var.DOMAIN
+  statuses = ["ISSUED"]
+}
+
 resource "aws_cloudfront_distribution" "cloud_resume" {
-  source              = "terraform-aws-modules/cloudfront/aws"
-  origin = {
+  origin {
     origin_id           = aws_s3_bucket.cloud_resume.bucket
     domain_name  = aws_s3_bucket.cloud_resume.bucket_domain_name
   }
 
   default_root_object = var.DEFAULT_ROOT_OBJECT
   aliases             = ["${var.PROJECT_NAME}.${var.DOMAIN}"]
+
+  enabled             = true
+  is_ipv6_enabled     = true
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.cloud_resume.bucket
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA"]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = data.aws_acm_certificate.issued
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2019"
+    ssl_support_method             = "sni-only"
+  }
+
 }
 
 data "aws_api_gateway_rest_api" "api" {
