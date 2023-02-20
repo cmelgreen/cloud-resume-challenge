c0eac7f4700908fd72e130f091f10ebba1dc3592
cmelgreen <cmelgreen@gmail.com>
2023-02-20 01:18:06 -0600

    update lambda cors

--- backend/visitor_count.py
+++ backend/visitor_count.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            "Access-Control-Allow-Origin": "cloud-resume.cmelgreen.com",
+            "Access-Control-Allow-Origin": "https://cloud-resume.cmelgreen.com",
         },
         'body': response['Attributes']['visitorCount']
     }
