62bb8088102e0b9907e42c6a763281886e1a2b90
cmelgreen <cmelgreen@gmail.com>
2023-02-20 00:35:38 -0600

    add cors to lambda since gateway is proxy

--- backend/visitor_count.py
+++ backend/visitor_count.py
@@ -18,5 +18,8 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "cloud-resume.cmelgreen.com",
+        },
         'body': response['Attributes']['visitorCount']
     }
